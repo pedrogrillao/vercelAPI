8289827b59b9d49c982fd66148508a626996775d
pedrogrillao <108986586+pedrogrillao@users.noreply.github.com>
2024-09-10 10:06:51 -0300

    v2?

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-import express from "express";
-import { montadoras, veiculos } from "./dados.js";
+import express from 'express';
+
+import { montadoras, veiculos } from './dados.js';
 
 const app = express();
 
-app.get('/', (req, res)=>{
-    res.json({"status":"ok"});
+app.get('/',(req,res)=>{
+    res.json({"status": "Server OK"});
+})
+app.get('/veiculos',(req,res)=>{
+    res.json(veiculos);
 })
 
-app.listen(3030);
+app.get('/montadoras',(req,res)=>{
+    res.json(montadoras);
+})
+
+const PORT = 3030; 
+app.listen(PORT,()=>{
+    console.log('Sistema inicializado: ', `Acesso: http://localhost:${PORT}`);
+});
